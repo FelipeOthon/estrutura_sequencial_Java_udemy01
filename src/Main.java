1b85037f0819b2a55f30f44d991c1e38837e3d8b
FelipeOthon <felipeosantiago@gmail.com>
2022-10-15 14:22:39 -0300

    enviar calculo pi

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Locale.setDefault(Locale.US);
+        Scanner scanner = new Scanner(System.in);
+
+
+        double raio, area, pi = 3.14159;
+
+        System.out.printf("Considerando que Pi é igual %.5f,Digite o valor do Raio: ", pi);
+
+        raio = scanner.nextDouble();
+        area = pi * raio * raio;
+
+
+        System.out.printf("area = %.4f%n", area );
+
+
+
+        scanner.close();
+
+
     }
 }
